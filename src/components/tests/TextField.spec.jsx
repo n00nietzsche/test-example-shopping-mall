47871b3477b9037ca038e3a8aa93c4fe00d87623
jakeseo <n00nietzsche@gmail.com>
2024-03-06 00:02:22 +0900

    TextField 에 대한 className 테스트 추가

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,6 +4,9 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-it('sample test.', async () => {
-  expect(1).toBe(1);
+it('클래스 이름이 올바르게 추가되는지 확인', async () => {
+  await render(<TextField className="my-class" />);
+  expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
+    'my-class',
+  );
 });
