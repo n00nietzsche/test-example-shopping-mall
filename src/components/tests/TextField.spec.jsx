a53bda021b1353b4ae1975141a98adc071fc3c01
jakeseo <n00nietzsche@gmail.com>
2024-03-06 00:08:16 +0900

    테스트 이름 변경

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-it('클래스 이름이 올바르게 추가되는지 확인', async () => {
+it('className prop 으로 설정한 css class 가 적용된다', async () => {
   await render(<TextField className="my-class" />);
   expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
     'my-class',
