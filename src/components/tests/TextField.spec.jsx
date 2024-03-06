4362c2a768cc554c736a4010dc971e3facf854f6
jakeseo <n00nietzsche@gmail.com>
2024-03-06 21:42:50 +0900

    test: TextField 관련 테스트 추가

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,12 +4,30 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-it('className prop 으로 설정한 css class 가 적용된다', async () => {
-  await render(<TextField className="my-class" />);
+describe('TextField 엘리먼트 테스트', () => {
+  it('className prop 으로 설정한 css class 가 적용된다', async () => {
+    await render(<TextField className="my-class" />);
 
-  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
 
-  screen.debug();
+    screen.debug();
 
-  expect(textInput).toHaveClass('my-class');
+    expect(textInput).toHaveClass('my-class');
+  });
+
+  it('기본 placeholder "텍스트를 입력해 주세요."가 노출된다.', async () => {
+    await render(<TextField className="my-class" />);
+
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    expect(textInput).toBeInTheDocument();
+  });
+
+  it('placeholder prop 에 따라 placeholder 가 변경된다.', async () => {
+    await render(<TextField placeholder={'상품명을 입력해 주세요.'} />);
+
+    const textInput = screen.getByPlaceholderText('상품명을 입력해 주세요.');
+
+    expect(textInput).toBeInTheDocument();
+  });
 });
