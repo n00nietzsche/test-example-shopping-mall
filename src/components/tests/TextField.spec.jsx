b7fce72389c267add1f9db20f312c02fd2d4c5dd
jakeseo <n00nietzsche@gmail.com>
2024-03-22 01:29:36 +0900

    학습을 위해 작성했던 Before Each 제거

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,13 +4,9 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-// 매번 my-class 란 class 가 적용된 컴포넌트를 렌더링
-beforeEach(async () => {
-  await render(<TextField className="my-class" />);
-});
-
 describe('TextField 엘리먼트 테스트', () => {
   it('className prop 으로 설정한 css class 가 적용된다', async () => {
+    await render(<TextField className="my-class" />);
     const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
 
     screen.debug();
@@ -20,12 +16,19 @@ describe('TextField 엘리먼트 테스트', () => {
 
   describe('placeholder 테스트', () => {
     it('기본 placeholder "텍스트를 입력해 주세요."가 노출된다.', async () => {
+      await render(<TextField className="my-class" />);
       const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
 
       expect(textInput).toBeInTheDocument();
     });
 
     it('placeholder prop 에 따라 placeholder 가 변경된다.', async () => {
+      await render(
+        <TextField
+          className="my-class"
+          placeholder={'상품명을 입력해 주세요.'}
+        />,
+      );
       const textInput = screen.getByPlaceholderText('상품명을 입력해 주세요.');
 
       expect(textInput).toBeInTheDocument();
