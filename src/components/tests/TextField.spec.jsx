2130f3885017438a031110368b46d7d219f1a8b2
jakeseo <n00nietzsche@gmail.com>
2024-03-22 01:54:57 +0900

    학습을 위한 여러 DOM API 및 이벤트 테스트

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
 import TextField from '@/components/TextField';
@@ -33,5 +34,58 @@ describe('TextField 엘리먼트 테스트', () => {
 
       expect(textInput).toBeInTheDocument();
     });
+  });
+});
+
+it('텍스트를 입력하면 onChange prop으로 등록한 함수가 호출된다.', async () => {
+  const spy = vi.fn(); // 스파이 함수
+
+  const { user } = await render(<TextField onChange={spy} />);
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  // test 라는 글자를 입력한다.
+  await user.type(textInput, 'test');
+
+  // test 가 호출된다. -> 스파이 함수를 통해 알 수 있음
+  expect(spy).toHaveBeenCalled('test');
+});
+
+it('엔터키를 입력하면 onEnter prop 으로 등록한 함수가 호출된다.', async () => {
+  const spy = vi.fn();
+
+  const { user } = await render(<TextField onEnter={spy} />);
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  // test 라는 글자를 입력한다.
+  await user.type(textInput, 'test{Enter}');
+
+  // test 가 호출된다. -> 스파이 함수를 통해 알 수 있음
+  expect(spy).toHaveBeenCalled('test');
+});
+
+it('focus 가 활성화되면 onFocus prop 으로 등록한 함수를 호출한다.', async () => {
+  const spy = vi.fn();
+  // 포커스 활성화 방법
+  //   탭키로 인풋 요소로 이동
+  //   인풋 요소를 클릭 (가장 보편적)
+  //   textInput.focus() API 를 직접 호출
+
+  await render(<TextField onFocus={spy} />);
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  await userEvent.click(textInput);
+
+  expect(spy).toHaveBeenCalled();
+});
+
+it('focus 가 활성화되면 border 스타일을 변경한다.', async () => {
+  await render(<TextField />);
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  await userEvent.click(textInput);
+
+  expect(textInput).toHaveStyle({
+    borderWidth: 2,
+    borderColor: 'rgb(25, 118, 210)',
   });
 });
